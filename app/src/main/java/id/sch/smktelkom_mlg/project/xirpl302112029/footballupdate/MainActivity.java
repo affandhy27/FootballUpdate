7f2348aa458db62b4957e4892717f27fcff64246
Affandhy <02_xirpl3.smktelkom-mlg.sch.id>
2016-11-16 20:36:28 +0700

    Webview

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl302112029/footballupdate/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl302112029/footballupdate/MainActivity.java
@@ -2,12 +2,29 @@ package id.sch.smktelkom_mlg.project.xirpl302112029.footballupdate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class MainActivity extends AppCompatActivity {
+    private WebView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webview);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://www.sundul.com/berita-bola/");
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
